2a1647472cad97c4bae433958ac2b06f04071511
Yaren Kose <ksyarenn@gmail.com>
2023-12-23 02:37:07 +0300

    exercise5-8 added

---
+++ c-programming-exercises/exercise5-8/exercise5-8.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int prime; 
+	prime = 777;
+	printf("First number is %d\n", prime);
+	prime = 888;
+	printf("Second number is %d\n", prime);
+	prime = 999;
+	printf("Third number is %d\n", prime);
+	return 0; 
+}
