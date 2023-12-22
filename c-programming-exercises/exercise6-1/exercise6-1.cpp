4d8f413a87f9c8d3144f8e7cffd8a9a09dc54458
Yaren Kose <ksyarenn@gmail.com>
2023-12-23 02:44:33 +0300

    exercise exercise6-1 added

---
+++ c-programming-exercises/exercise6-1/exercise6-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	const int v = 3;
+
+	printf("The value is %d\n", v);
+	printf("And %d is the value \n", v);
+	printf("It's not %d\n", v + 1);
+	printf("It's not %d\n", v - 1);
+	printf("No, the value is %d\n", v);
+	return 0; 
+}
