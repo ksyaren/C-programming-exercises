ceefd852766451ccf24eecf6c6724ae4fb1fc3f2
Yaren Kose <ksyarenn@gmail.com>
2023-12-23 02:33:06 +0300

    exercise5-7 added

---
+++ c-programming-exercises/exercise5-7/exercise5-7.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int firsNumber = 777;
+	int secondNumber = 888;
+	int thirdNumber = 999;
+
+	printf(" First number is %d\n secondNumber is %d\n thirdNumber is %d\n",firsNumber,secondNumber,thirdNumber);
+	return 0;
+}
