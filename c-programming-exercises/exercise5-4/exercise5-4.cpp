cf2efbd587bf3422324241681f82d358403f128e
Yaren Kose <ksyarenn@gmail.com>
2023-12-23 02:10:40 +0300

    exercise5-4 added

---
+++ c-programming-exercises/exercise5-4/exercise5-4.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	char c;
+	int i;
+	float f;
+	double d;
+
+	c = 'Y';
+	i = 77;
+	f = 19.0;
+	d = 20000.009; 
+
+	printf("%c\n", c);
+	printf("%d\n", i);
+	printf("%f\n", f);
+	printf("%f\n", d);
+	return 0; 
+}
