97d5c95fa49be1fefdb39871d58e8e7001442f7f
Yaren Kose <ksyarenn@gmail.com>
2023-12-23 02:18:35 +0300

    exercise5-5 added

---
+++ c-programming-exercises/exercise5-5/exercise5-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int example;
+	example = 22;
+
+	printf("The value of example is %d.\n", example);
+	printf("The value of example plus 16 is %d.\n", example + 16);
+	printf("The value of example times itself is %d.\n", example * example);
+	return 0;
+
+}
